76538f531e8f6bc2f975fbb3e7011cafe3cae89c
taggeddev <denis.mikov.06@gmail.com>
2021-03-20 13:32:42 +0300

    Subclasses created

---
+++ Bot/Types/Magic/Mage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bot.Types.Magic
+{
+    class Mage : Archetype
+    {
+        public Mage(string name, int id, string race, int health, int damage, float luck, float dodge)
+        {
+            _name = name;
+            _id = id;
+            _health = health + 775;
+            _damage = damage + 60;
+            _luck = luck + 0.21f;
+            _dodge = dodge + 0.17f;
+        }
+    }
+}
---
+++ Bot/Types/Melee/Tamplier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bot.Types.Melee
+{
+    class Tamplier : Archetype
+    {
+        public Tamplier(string name, int id, string race, int health, int damage, float luck, float dodge)
+        {
+            _name = name;
+            _id = id;
+            _health = health + 775;
+            _damage = damage + 60;
+            _luck = luck + 0.21f;
+            _dodge = dodge + 0.17f;
+        }
+    }
+}
