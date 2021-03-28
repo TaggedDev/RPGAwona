57a0656940cae5de1885448410304e3ba1a6b0bb
taggeddev <denis.mikov.06@gmail.com>
2021-03-28 22:37:17 +0300

    Added new field with constructor

--- Bot/Classes/Types/Faith/Acolyte.cs
+++ Bot/Classes/Types/Faith/Acolyte.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using Bot.Modules;
 using Microsoft.Data.Sqlite;
 
 namespace Bot.Types
 {
     class Acolyte : Archetype
     {
+
+        Provider provider = new Provider();
+
         public Acolyte(string name, ulong id, int health, int defence, int damage, float luck, float dodge)
         {
             _name = name;
@@ -19,6 +23,7 @@ namespace Bot.Types
         {
             _name = name;
             _id = id;
+            _lvl = Convert.ToInt32(provider.GetFieldAwonaByID("level", Convert.ToString(Id), "discord_id", "users"));
             _health = 250;
             _protection = .4f;
             _multiplier = 1.45f;
