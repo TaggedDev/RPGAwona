bebb863579eeb8ee7b47ec74e548b6618091e7d4
taggeddev <denis.mikov.06@gmail.com>
2021-03-29 23:15:15 +0300

    Deleted folder Japan and renamed with serenity

---
+++ Bot/Classes/Types/Serenity/Asigaru.cs
@@ -0,0 +1,26 @@
+﻿using Bot.Modules;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bot.Types.Serenity
+{
+    class Asigaru : Archetype
+    {
+        readonly Provider provider = new Provider();
+
+        public Asigaru(string name, ulong id)
+        {
+            _name = name;
+            _id = id;
+            _lvl = Convert.ToInt32(provider.GetFieldAwonaByID("level", Convert.ToString(Id), "discord_id", "users"));
+            _health = 1100;
+            _damage = 110;
+            _armor = 80;
+            _protection = 1.5f;
+            _dodge = 0.2f;
+            _luck = 0.6f;
+            _multiplier = 1.60f;
+        }
+    }
+}
